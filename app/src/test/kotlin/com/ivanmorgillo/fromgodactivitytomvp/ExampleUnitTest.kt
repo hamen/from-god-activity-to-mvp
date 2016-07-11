2e79eba4756268968ba82525af1aa9d9d1e7fc83
Ivan Morgillo <imorgillo@gmail.com>
2016-07-11 11:51:09 +0200

    Add Kotlin

---
+++ app/src/test/kotlin/com/ivanmorgillo/fromgodactivitytomvp/ExampleUnitTest.kt
@@ -0,0 +1,15 @@
+package com.ivanmorgillo.fromgodactivitytomvp
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+
+class ExampleUnitTest {
+
+    @Test
+    @Throws(Exception::class)
+    fun addition_isCorrect() {
+        assertEquals(4, (2 + 2).toLong())
+    }
+}
---
+++ app/src/test/kotlin/com/ivanmorgillo/fromgodactivitytomvp/mvp/ui/MvpPresenterTest.kt
@@ -0,0 +1,26 @@
+package com.ivanmorgillo.fromgodactivitytomvp.mvp.ui
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class MvpPresenterTest {
+
+    @Before
+
+    fun setUp() {
+
+    }
+
+    @After
+    fun tearDown() {
+
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun loadQuestions() {
+
+    }
+
+}
